ae10cb78194b9bd989bb4d18f865caa68d49b425
Liam Frija-Altarac <liamfa@beluga1.int.ets1.calculquebec.ca>
2025-03-12 16:23:05 -0400

    f

--- slurm_run.sh
+++ slurm_run.sh
@@ -5,7 +5,7 @@
 #SBATCH --gpus-per-node=1
 #SBATCH --cpus-per-gpu=1  # 6*4: There are 24 CPU cores on P100 Cedar GPU nodes
 #SBATCH --mem=24G         # Request the full memory of the node
-#SBATCH --time=00:07:00
+#SBATCH --time=00:20:00
 #SBATCH --wait-all-nodes=1
 #SBATCH --output=%N-%j.out
 #SBATCH --mail-user=liam.frija-altarac.1@ens.etsmtl.ca
@@ -25,13 +25,16 @@ source $SLURM_TMPDIR/env/bin/activate
 # Upgrade pip and install required packages
 pip install --no-index --upgrade pip
 pip install --no-index torch==2.4.1 torchvision==0.19.1 torchaudio==2.4.1
-pip install -r requirements.txt
-pip install omegaconf scipy trimesh imageio joblib pandas flash-attn
+pip install  --no-index  -r requirements.txt
+pip install --no-index omegaconf scipy trimesh imageio joblib pandas flash-attn
 
 # Install Open3D from the official source
-pip install open3d -f https://www.open3d.org/docs/latest/getting_started.html
+#pip install  open3d -f https://www.open3d.org/docs/latest/getting_started.html
 
 # Do not install opencv-contrib-python from pip because we're using the system's OpenCV.
 # pip install --no-index opencv-contrib-python
 
-pip install xformers==0.0.28.post1 --no-build-isolation
+pip install  --no-index xformers --no-build-isolation
+
+
+python evalBooster.py
