b24398a21a121ae2b02d73bb40638b4a231abe12
Liam Frija-Altarac <liamfa@beluga1.int.ets1.calculquebec.ca>
2025-03-12 16:31:02 -0400

    f

--- slurm_run.sh
+++ slurm_run.sh
@@ -26,7 +26,8 @@ source $SLURM_TMPDIR/env/bin/activate
 pip install --no-index --upgrade pip
 pip install --no-index torch==2.4.1 torchvision==0.19.1 torchaudio==2.4.1
 pip install  --no-index  -r requirements.txt
-pip install --no-index omegaconf scipy trimesh imageio joblib pandas flash-attn
+pip install --no-index omegaconf scipy trimesh imageio joblib pandas timm einops
+
 
 # Install Open3D from the official source
 #pip install  open3d -f https://www.open3d.org/docs/latest/getting_started.html
